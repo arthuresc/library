b5b18f0d0664e67fa3bd916b926a336681cf1355
Arthur Escalera <arthurescalera@gmail.com>
2026-09-03 18:53:58 -0300

    att

--- aulas/Algoritmos e Programação para IA/Ex04_ArthurEscalera.py
+++ aulas/Algoritmos e Programação para IA/Ex04_ArthurEscalera.py
@@ -14,13 +14,37 @@ def soliciteIdade(pIdade):
   idade = pIdade
   if idade < 12:
     print("Criança")
-  if idade <= 12 or :
-    print("Criança")
-  if idade < 12:
-    print("Criança")
-  if idade < 12:
-    print("Criança")
+  if idade <= 12 and idade >= 17:
+    print("Adolescente")
+  if idade <= 18 and idade >= 59:
+    print("Adulto")
+  if idade >= 60:
+    print("Idoso")
   
   
   
 soliciteIdade(78)
+soliciteIdade(13)
+soliciteIdade(34)
+
+
+# 2) Função chamada retorna_maior
+# Essa função deverá receber 2 números inteiros e voltar o valor
+# do maior valor.
+# Exemplo:
+# print(retorna_maior(3, 4)) # 4
+# print(retorna_maior(8, 5)) # 8
+
+def retorna_maior(a, b):
+  return a if a > b else b  
+
+retorna_maior(1, 2)
+retorna_maior(6, 2)
+
+# 3) Crie a função calc_desconto
+# 1
+# Receba o valor de uma compra (número de ponto flutuante):
+# - Até R$100 → 5% de desconto
+# - Até R$500 → 10% de desconto
+# - Acima de R$500 → 15% de desconto
+# Mostre o valor final com desconto.
