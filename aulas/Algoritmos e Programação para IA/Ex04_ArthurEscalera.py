c37e5604a4af0239b06ead41037b0b24972a51e4
Arthur Escalera <48338321+arthuresc@users.noreply.github.com>
2026-09-03 14:31:08 +0000

    att

---
+++ aulas/Algoritmos e Programação para IA/Ex04_ArthurEscalera.py
@@ -0,0 +1,26 @@
+# 1) Crie um programa em Python que:
+# - Solicite ao usuário sua idade.
+# - Verifique:
+  # Se a idade for menor que 0 → imprima "Idade inválida"
+  # Se for menor que 12 → imprima "Criança"
+  # Se for entre 12 e 17 → imprima "Adolescente"
+  # Se for entre 18 e 59 → imprima "Adulto"
+  # Se for 60 ou mais → imprima "Idoso"
+# def soliciteIdade(pIdade = None):
+#   idade = pIdade or input("Digite sua idade: ");
+#   print("Teste final", idade)
+
+def soliciteIdade(pIdade):
+  idade = pIdade
+  if idade < 12:
+    print("Criança")
+  if idade <= 12 or :
+    print("Criança")
+  if idade < 12:
+    print("Criança")
+  if idade < 12:
+    print("Criança")
+  
+  
+  
+soliciteIdade(78)
---
+++ aulas/Introdução à Computação/aula intro - 03-09-2026.md
@@ -0,0 +1,25 @@
+# Aula de aula intro
+
+## Principal
+
+- ...
+
+## O que entendi:
+
+- ...
+
+## O que não entendi:
+
+- ...
+
+## TODOs (tarefas):
+
+- [ ] ...
+
+## Glossário:
+
+- ...
+
+## Notas e Rascunhos:
+
+...
